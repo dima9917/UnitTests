ad04045cab40ac4b1de62fa1941ced66ff8afd86
dima9917 <dima9917@gmail.com>
2018-08-08 08:39:20 -0700

    S6L31: Mocking Methods

---
+++ UnitTests2/TestDoubles.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests2
+{
+    class TestDoubles
+    {
+    }
+
+    [TestFixture]
+    public class MethodSamples
+    {
+        [Test]
+        public void OrdinaryMethodCalls()
+        {
+            var mock = new Mock<IFoo>();
+
+            mock.Setup(foo => foo.DoSomething("ping")).Returns(true);
+            mock.Setup(foo => foo.DoSomething(It.IsIn("pong", "foo"))).Returns(false);
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(mock.Object.DoSomething("ping"));
+                Assert.IsFalse(mock.Object.DoSomething("pong"));
+                Assert.IsFalse(mock.Object.DoSomething("foo"));
+            });
+        }
+    }
+
+    public interface IFoo
+    {
+        bool DoSomething(string v);
+    }
+}
