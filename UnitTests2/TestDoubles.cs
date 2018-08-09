ad1073d482a176a1d6becdd95fbdab3998adb595
dima9917 <dima9917@gmail.com>
2018-08-08 18:30:11 -0700

    S6L33

--- UnitTests2/TestDoubles.cs
+++ UnitTests2/TestDoubles.cs
@@ -30,10 +30,26 @@ namespace UnitTests2
                 Assert.IsFalse(mock.Object.DoSomething("foo"));
             });
         }
+
+        [Test]
+        public void ArgumentDependentMatching()
+        {
+            var mock = new Mock<IFoo>();
+
+            mock.Setup(foo => foo.DoSomething(It.IsAny<string>()))
+                .Returns(true);
+            mock.Setup(foo => foo.Add(It.Is<int>(x => x % 2 == 0)))
+                .Returns(true);
+            mock.Setup(foo => foo.Add(It.IsInRange<int>(1, 10, Range.Inclusive)))
+                .Returns(false);
+            mock.Setup(foo => foo.DoSomething(It.IsRegex("[a-z]+")))
+                .Returns(false);
+        }
     }
 
     public interface IFoo
     {
+        bool Add(int v);
         bool DoSomething(string v);
     }
 }
