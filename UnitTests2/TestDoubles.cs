69ada75160f475c509d81a897db2c7b3f2829b0a
dima9917 <dima9917@gmail.com>
2018-08-09 11:12:03 -0700

    S6L33

--- UnitTests2/TestDoubles.cs
+++ UnitTests2/TestDoubles.cs
@@ -45,11 +45,46 @@ namespace UnitTests2
             mock.Setup(foo => foo.DoSomething(It.IsRegex("[a-z]+")))
                 .Returns(false);
         }
+
+        [Test]
+        public void OutAndRefArguments()
+        {
+            var mock = new Mock<IFoo>();
+            var requiredOutput = "ok";
+            mock.Setup(foo => foo.TryParse("ping", out requiredOutput))
+                .Returns(true);
+
+            string result;
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(mock.Object.TryParse("ping", out result));
+                Assert.That(result, Is.EqualTo(requiredOutput));
+
+                var thisShouldBeFalse = mock.Object.TryParse("pong", out result);
+                Console.WriteLine(thisShouldBeFalse);
+                Console.WriteLine(result);
+            });
+
+            var bar = new Bar();
+            mock.Setup(foo => foo.Submit(ref bar)).Returns(true);
+            Assert.That(mock.Object.Submit(ref bar), Is.EqualTo(true));
+
+            var otherBar = new Bar();
+            Assert.IsFalse(mock.Object.Submit(ref otherBar));
+
+            //bar and otherbar will always return different results from Moq.
+        }
     }
 
     public interface IFoo
     {
         bool Add(int v);
         bool DoSomething(string v);
+        bool TryParse(string v, out string requiredOutput);
+        bool Submit(ref Bar bar);
+    }
+
+    public class Bar
+    {
     }
 }
